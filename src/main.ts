4b6ad19756fbcb218865748fa999b04e7e7dedaf
DevFelipreis <lfreis.contato@gmail.com>
2024-02-06 13:08:15 -0300

    fix: router configurado

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+console.clear();
+
 import "dotenv/config";
 
 import express from "express";
+import rotas from "./rotas";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(rotas)
 
 app.listen(3000, () => {
     console.log("Servidor iniciado na porta 3000");
---
+++ src/rotas.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const rotas = Router();
+
+rotas.get("/", (req, res) => {
+
+})
+
+export default rotas;
